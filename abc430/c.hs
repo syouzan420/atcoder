b6f97cd81479b26d776598ba95d9d021d1522f4c
syouzan420 <syouzanponkikki@yahoo.co.jp>
2025-11-05 12:46:56 +0900

    update

--- abc430/c.hs
+++ abc430/c.hs
@@ -1,19 +1,40 @@
 import qualified Data.IntMap as M
 import Data.IntMap ((!))
-import Data.List (scanl')
+import qualified Data.ByteString.Char8 as B
+import Data.List (unfoldr,scanl')
 
 ints :: IO [Int]
-ints = map read . words <$> getLine
+ints = unfoldr (B.readInt . B.dropSpace) <$> B.getLine
 
 main :: IO ()
 main = do
   [n,a,b] <- ints
   s <- getLine
-  let mpa = M.fromList (zip [0..n] (scanl' (\i ch->if ch=='a' then i+1 else i) 0 s))
-  let mpb = M.fromList (zip [0..n] (scanl' (\i ch->if ch=='b' then i+1 else i) 0 s))
-  let lr = [(l,r)|l<-[1..n],r<-[l..n]]
-  let res = length $ filter (check a b mpa mpb) lr
+  let mpa = M.fromList (zip [0..(n+1)] (scanl' (\i ch->if ch=='a' then i+1 else i) 0 (s++"b")))
+  let mpb = M.fromList (zip [0..(n+1)] (scanl' (\i ch->if ch=='b' then i+1 else i) 0 (s++"b")))
+  let res = check 1 (n+1) a b mpa mpb 
   print res
 
-check :: Int -> Int -> M.IntMap Int -> M.IntMap Int -> (Int,Int) -> Bool
-check a b mpa mpb (l,r) = (mpa!r - mpa!(l-1) >= a) && (mpb!r - mpb!(l-1) < b)
+check :: Int -> Int -> Int -> Int -> M.IntMap Int -> M.IntMap Int -> Int
+check l r a b mpa mpb
+    | l>r = 0 
+    | otherwise = let mia = mpa!(l-1)
+                      mib = mpb!(l-1)
+                      m = (l+r) `div` 2
+                      ma = if mia+a>mpa!r then 0 else getra l m r (mia+a) mpa 
+                      mb = getrb l m r (mib+b) mpb
+                      mn = if ma/=0 && mb>=ma then mb-ma+1 else 0
+                   in mn+check (l+1) r a b mpa mpb
+
+getra :: Int -> Int -> Int -> Int -> M.IntMap Int -> Int
+getra l m r al mpa 
+    | mpa!m >= al = if m==l then l 
+                            else let nm = (l+m) `div` 2 in getra l nm m al mpa 
+    | otherwise = if m==l then r
+                          else let nm = (m+r) `div` 2 in getra m nm r al mpa
+
+getrb :: Int -> Int -> Int -> Int -> M.IntMap Int -> Int
+getrb l m r bl mpb 
+    | m==l = l
+    | mpb!m < bl = let nm = (r+m) `div` 2 in getrb m nm r bl mpb 
+    | otherwise =  let nm = (m+l) `div` 2 in getrb l nm m bl mpb
--- abc430/c1.hs
+++ abc430/c1.hs
@@ -10,31 +10,31 @@ main :: IO ()
 main = do
   [n,a,b] <- ints
   s <- getLine
-  let mpa = M.fromList (zip [0..n] (scanl' (\i ch->if ch=='a' then i+1 else i) 0 s))
-  let mpb = M.fromList (zip [0..n] (scanl' (\i ch->if ch=='b' then i+1 else i) 0 s))
-  let res = check n a b mpa mpb 1 
+  let mpa = M.fromList (zip [0..(n+1)] (scanl' (\i ch->if ch=='a' then i+1 else i) 0 (s++"b")))
+  let mpb = M.fromList (zip [0..(n+1)] (scanl' (\i ch->if ch=='b' then i+1 else i) 0 (s++"b")))
+  let res = check 1 (n+1) a b mpa mpb 
   print res
 
-check :: Int -> Int -> Int -> M.IntMap Int -> M.IntMap Int -> Int -> Int
-check n a b mpa mpb l
-    | l>n = 0 
+check :: Int -> Int -> Int -> Int -> M.IntMap Int -> M.IntMap Int -> Int
+check l r a b mpa mpb
+    | l>r = 0 
     | otherwise = let mia = mpa!(l-1)
                       mib = mpb!(l-1)
-                      r = (n+l) `div` 2
-                      ra = if mia+a>mpa!n then 0 else getra l r n (mia+a) mpa 
-                      rb = getrb l r n (mib+b) mpb
-                      rn = if ra/=0 && rb>=ra then rb-ra+1 else 0
-                   in rn+check n a b mpa mpb (l+1)
+                      m = (l+r) `div` 2
+                      ma = if mia+a>mpa!r then 0 else getra l m r (mia+a) mpa 
+                      mb = getrb l m r (mib+b) mpb
+                      mn = if ma/=0 && mb>=ma then mb-ma+1 else 0
+                   in mn+check (l+1) r a b mpa mpb
 
 getra :: Int -> Int -> Int -> Int -> M.IntMap Int -> Int
-getra l r n al mpa 
-    | mpa!r >= al = if r==l then l 
-                            else let nr = (r+l) `div` 2 in getra l nr r al mpa 
-    | otherwise = if r==l then n
-                          else let nr = (n+r) `div` 2 in getra r nr n al mpa
+getra l m r al mpa 
+    | mpa!m >= al = if m==l then l 
+                            else let nm = (l+m) `div` 2 in getra l nm m al mpa 
+    | otherwise = if m==l then r
+                          else let nm = (m+r) `div` 2 in getra m nm r al mpa
 
 getrb :: Int -> Int -> Int -> Int -> M.IntMap Int -> Int
-getrb l r n bl mpb 
-    | r==l = l
-    | mpb!r < bl = let nr = (n+r) `div` 2 in getrb r nr n bl mpb 
-    | otherwise =  let nr = (r+l) `div` 2 in getrb l nr r bl mpb
+getrb l m r bl mpb 
+    | m==l = l
+    | mpb!m < bl = let nm = (r+m) `div` 2 in getrb m nm r bl mpb 
+    | otherwise =  let nm = (m+l) `div` 2 in getrb l nm m bl mpb
